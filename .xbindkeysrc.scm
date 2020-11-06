86ea4d47696bbbc73737e92c654ecb8640586897
vlad-ku <tmnt-95@yandex.ru>
2020-11-06 10:18:48 +0800

    un&down

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,10 +1,7 @@
 ; mod1 - Alt
 ; mod4 - win
 
-; 0 - delete
-(xbindkey '(mod4 d)       "xdotool   keyup --delay 0 d   keyup --delay 0 Super   key --delay 0 Delete   keydown --delay 0 Super")
-
-; 0 - стрелки
+; 1 - стрелки
 (xbindkey '(mod4 j)       "xdotool   keyup --delay 0 j   keyup --delay 0 Super   key --delay 0 Down    keydown --delay 0 Super")
 (xbindkey '(Shift mod4 j) "xdotool   keyup --delay 0 j   keyup --delay 0 Super   key --delay 0 Down    keydown --delay 0 Super")
 (xbindkey '(mod4 k)       "xdotool   keyup --delay 0 k   keyup --delay 0 Super   key --delay 0 Up      keydown --delay 0 Super")
@@ -25,3 +22,9 @@
 (xbindkey '(Shift mod4 w) "xdotool   keyup --delay 0 w   keyup --delay 0 Super   keydown --delay 0 Ctrl   key --delay 0 Right   keyup --delay 0 Ctrl   keydown --delay 0 Super")
 (xbindkey '(mod4 b)       "xdotool   keyup --delay 0 b   keyup --delay 0 Super   keydown --delay 0 Ctrl   key --delay 0 Left    keyup --delay 0 Ctrl   keydown --delay 0 Super")
 (xbindkey '(Shift mod4 b) "xdotool   keyup --delay 0 b   keyup --delay 0 Super   keydown --delay 0 Ctrl   key --delay 0 Left    keyup --delay 0 Ctrl   keydown --delay 0 Super")
+
+; 4 - вертикальные варп прыжки (х5)
+(xbindkey '(mod4 i)       "xdotool   keyup --delay 0 i   keyup --delay 0 Super   key --delay 0 --repeat 5  Up      keydown --delay 0 Super")
+(xbindkey '(Shift mod4 i) "xdotool   keyup --delay 0 i   keyup --delay 0 Super   key --delay 0 --repeat 5  Up      keydown --delay 0 Super")
+(xbindkey '(mod4 o)       "xdotool   keyup --delay 0 o   keyup --delay 0 Super   key --delay 0 --repeat 5  Down    keydown --delay 0 Super")
+(xbindkey '(Shift mod4 o) "xdotool   keyup --delay 0 o   keyup --delay 0 Super   key --delay 0 --repeat 5  Down    keydown --delay 0 Super")
